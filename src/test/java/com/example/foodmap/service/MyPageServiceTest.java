cd109fcf15649b8f7fd7d2708c608be20b331b02
HundredCleanWater <maxmp9904@gmail.com>
2022-01-12 11:29:41 +0900

    feat : mylistTestCode success

--- src/test/java/com/example/foodmap/service/MyPageServiceTest.java
+++ src/test/java/com/example/foodmap/service/MyPageServiceTest.java
@@ -2,8 +2,10 @@ package com.example.foodmap.service;
 
 import com.example.foodmap.dto.Restaurant.RestaurantSaveRequestDto;
 import com.example.foodmap.dto.meeting.MeetingCreatRequestDto;
+import com.example.foodmap.dto.mypage.MyLikeResponseDto;
 import com.example.foodmap.dto.mypage.MyReviewResponseDto;
 import com.example.foodmap.dto.review.ReviewRequestDto;
+import com.example.foodmap.exception.CustomException;
 import com.example.foodmap.model.*;
 import com.example.foodmap.repository.*;
 import com.example.foodmap.security.UserDetailsImpl;
@@ -18,6 +20,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Transactional
@@ -64,7 +68,7 @@ class MyPageServiceTest {
         user1 = new User(
                 "백정수",
                 "a111",
-                6L,
+                100L,
                 "a@b.com",
                 UserRoleEnum.USER,
                 1L,
@@ -76,10 +80,10 @@ class MyPageServiceTest {
         userDetails1 = new UserDetailsImpl(user1);
 
         Location location2 = new Location("강북구", 34.31, 34.21);
-        user1 = new User(
+        user2 = new User(
                 "이한울",
                 "a111",
-                7L,
+                200L,
                 "a@bs.com",
                 UserRoleEnum.USER,
                 2L,
@@ -89,20 +93,6 @@ class MyPageServiceTest {
         );
         userRepository.save(user2);
         userDetails2 = new UserDetailsImpl(user2);
-
-        //식당 저장
-        RestaurantSaveRequestDto restaurantSaveRequestDto1 = new RestaurantSaveRequestDto(
-                "엽떡",
-                22.33,
-                33.44,
-                "서울특별시 강동구",
-                "분식",
-                "판매",
-                "허파만",
-                "밀떡"
-        );
-        restaurant1 = new Restaurant(restaurantSaveRequestDto1, null, user1, location1);
-        restaurantRepository.save(restaurant1);
 
         RestaurantSaveRequestDto restaurantSaveRequestDto2 = new RestaurantSaveRequestDto(
                 "안돼",
@@ -114,31 +104,71 @@ class MyPageServiceTest {
                 "허파만",
                 "밀떡"
         );
-        restaurant2 = new Restaurant(restaurantSaveRequestDto2, null, user1, location1);
+        restaurant1 = new Restaurant(restaurantSaveRequestDto2, null, user1, location1);
+        restaurantRepository.save(restaurant1);
+        restaurantLikes1 = new RestaurantLikes(1L, user1, restaurant1);
+        restaurantLikesRepository.save(restaurantLikes1);
+        RestaurantSaveRequestDto restaurantSaveRequestDto1 = new RestaurantSaveRequestDto(
+                "안돼",
+                22.33,
+                33.44,
+                "강동구",
+                "분식",
+                "판매안함",
+                "허파만",
+                "밀떡"
+        );
+        restaurant2 = new Restaurant(restaurantSaveRequestDto1, null, user1, location1);
+        restaurantRepository.save(restaurant2);
+        restaurantLikes2 = new RestaurantLikes(2L, user1, restaurant2);
+        restaurantLikesRepository.save(restaurantLikes2);
+
+//        //식당 저장
+//        RestaurantSaveRequestDto restaurantSaveRequestDto1 = new RestaurantSaveRequestDto(
+//                "엽떡",
+//                22.33,
+//                33.44,
+//                "서울특별시 강동구",
+//                "분식",
+//                "판매",
+//                "허파만",
+//                "밀떡"
+//        );
+//        restaurant1 = new Restaurant(restaurantSaveRequestDto1, null, user1, location1);
+//        restaurantRepository.save(restaurant1);
+//
+//        RestaurantSaveRequestDto restaurantSaveRequestDto2 = new RestaurantSaveRequestDto(
+//                "안돼",
+//                22.33,
+//                33.44,
+//                "강동구",
+//                "분식",
+//                "판매안함",
+//                "허파만",
+//                "밀떡"
+//        );
+//        restaurant2 = new Restaurant(restaurantSaveRequestDto2, null, user1, location1);
 
         //식당 찜하기저장
-        restaurantLikes1 = new RestaurantLikes(restaurantLikes1.getId(),user1, restaurant1);
-        restaurantLikesRepository.save(restaurantLikes1);
-        restaurantLikes2 = new RestaurantLikes(restaurantLikes2.getId(),user2, restaurant1);
-        restaurantLikesRepository.save(restaurantLikes1);
-        restaurantLikes3 = new RestaurantLikes(restaurantLikes3.getId(),user1, restaurant2);
-        restaurantLikesRepository.save(restaurantLikes3);
+
+//        restaurantLikes3 = new RestaurantLikes(restaurantLikes3.getId(),user1, restaurant2);
+//        restaurantLikesRepository.save(restaurantLikes3);
 
         //리뷰저장
         ReviewRequestDto reviewRequestDto1 = new ReviewRequestDto(
-                "이집 잘하네요.",
+                "이집 매우매우 잘하네요.",
                 "1",
                 "2"
         );
-        review1 = new Review(reviewRequestDto1, user1, restaurant1,"image1");
+        review1 = new Review(reviewRequestDto1, user1, restaurant1, null);
         reviewRepository.save(review1);
 
         ReviewRequestDto reviewRequestDto2 = new ReviewRequestDto(
-                "이집 별로네요.",
+                "이집 매우매우 별로네요.",
                 "5",
                 "3"
         );
-        review2 = new Review(reviewRequestDto2, user2, restaurant2,"image1");
+        review2 = new Review(reviewRequestDto2, user1, restaurant2, "image1");
         reviewRepository.save(review2);
 
         //모임저장
@@ -173,7 +203,9 @@ class MyPageServiceTest {
         meetingRepository.save(meeting2);
 
         //모임참가 저장
-        meetingParticipate = new MeetingParticipate(meeting1, userDetails2);
+        meetingParticipate = new MeetingParticipate(meeting1, userDetails1);
+        meetingParticipateRepository.save(meetingParticipate);
+        meetingParticipate = new MeetingParticipate(meeting2, userDetails1);
         meetingParticipateRepository.save(meetingParticipate);
 
     }
@@ -184,27 +216,119 @@ class MyPageServiceTest {
     @DisplayName("성공케이스")
     class sucess {
 
+        //region 리뷰 리스트
         @Test
         @DisplayName("리뷰 리스트")
         void 리뷰리스트() {
             // given
+            Location location1 = new Location("강서구", 12.23, 34.21);
+            user1 = new User(
+                    "백정수",
+                    "a111",
+                    6L,
+                    "a@b.com",
+                    UserRoleEnum.USER,
+                    1L,
+                    null,
+                    location1,
+                    "클린워터"
+            );
+            userRepository.save(user1);
+            userDetails1 = new UserDetailsImpl(user1);
+
             List<MyReviewResponseDto> myReviewList = new ArrayList<>();
+
+            ReviewRequestDto reviewRequestDto1 = new ReviewRequestDto(
+                    "이집 매우매우매우잘하네요.",
+                    "1",
+                    "2"
+            );
+            review1 = new Review(reviewRequestDto1, user1, restaurant1, "image1");
+            reviewRepository.save(review1);
+
+            ReviewRequestDto reviewRequestDto2 = new ReviewRequestDto(
+                    "이집 매우매우매우 별로네요.",
+                    "5",
+                    "3"
+            );
+            review2 = new Review(reviewRequestDto2, user1, restaurant1, "image1");
+            reviewRepository.save(review2);
+
             List<Review> reviewList = reviewRepository.findAllByUser(user1);
-            for (Review review : reviewList){
+            for (Review review : reviewList) {
                 MyReviewResponseDto myReviewResponseDto = MyReviewResponseDto.builder()
                         .reviewId(review.getId())
-                        .userId(user1.getId())
+                        .userId(review.getUser().getId())
                         .restaurantId(review.getRestaurant().getId())
                         .image(review.getImage())
                         .build();
                 myReviewList.add(myReviewResponseDto);
 
             }
-            assertThat(myReviewList.size()).isEqualTo(0);
+            assertThat(myReviewList.size()).isEqualTo(2);
+        }
+        //endregion
+
+        //region 좋아요리스트
+        @Test
+        @DisplayName("좋아요 리스트")
+        void 좋아요리스트() {
+            // given
+
+            //when
+            List<RestaurantLikes> restaurantLikesList = restaurantLikesRepository.findAllByUser(user1);
+
+            //then
+            assertThat(restaurantLikesList.size()).isEqualTo(2);
 
         }
-    }
-//endregion
+        //endregion
 
+        //region 내가 작성 한 식당
+        @Test
+        @DisplayName("내가 작성 한 식당")
+        void 작성식당() {
+            // given
+
+            //when
+            List<Restaurant> restaurantList = restaurantRepository.findAllByUser(user1);
+
+            //then
+            assertThat(restaurantList.size()).isEqualTo(2);
+        }
+        //endregion
+    }
+
+    //region 내가 참가 한 모임
+    @Test
+    @DisplayName("내가 참가 한 모임")
+    void 참가모임() {
+        // given
+
+        //when
+        List<MeetingParticipate> meetingList = meetingParticipateRepository.findAllByUser(user1);
+
+        //then
+        assertThat(meetingList.size()).isEqualTo(2);
+    }
+    //endregion
+
+    //region 내 리뷰 조회
+    @Test
+    @DisplayName("내 리뷰 조회")
+    void 리뷰조회() {
+        // given
+
+        //when
+        Review review = reviewRepository.findAllByUserAndId(user1, review1.getId());
+        //then
+        assertEquals(review1.getContent(),review.getContent());
+        assertEquals(review1.getReviewLike(),review.getReviewLike());
+        assertEquals(review1.getImage(),review.getImage());
+        assertEquals(review1.getSpicy(),review.getSpicy());
+        assertEquals(review1.getRestaurantTags(),review.getRestaurantTags());
+        assertEquals(review1.getId(),review.getId());
+        assertEquals(review1.getUser(),review.getUser());
+    }
 
 }
--- src/test/java/com/example/foodmap/service/ReviewServiceTest.java
+++ src/test/java/com/example/foodmap/service/ReviewServiceTest.java
@@ -4,6 +4,8 @@ import com.example.foodmap.dto.Restaurant.RestaurantSaveRequestDto;
 import com.example.foodmap.dto.review.ReviewAllResponseDto;
 import com.example.foodmap.dto.review.ReviewRequestDto;
 import com.example.foodmap.dto.review.ReviewUpdateRequestDto;
+import com.example.foodmap.exception.CustomException;
+import com.example.foodmap.exception.ErrorCode;
 import com.example.foodmap.model.*;
 import com.example.foodmap.repository.RestaurantRepository;
 import com.example.foodmap.repository.ReviewRepository;
@@ -21,6 +23,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Transactional
@@ -36,17 +40,24 @@ class ReviewServiceTest {
     @Autowired
     ReviewService reviewService;
 
-    private ReviewRequestDto reviewRequestDto;
+    private ReviewRequestDto reviewRequestDto1;
+    private ReviewRequestDto reviewRequestDto2;
+    private ReviewUpdateRequestDto reviewUpdateRequestDto1;
     private User user1;
     private User user2;
+    private User user3;
     private UserDetailsImpl userDetails1;
     private UserDetailsImpl userDetails2;
     private Restaurant restaurant1;
     private Restaurant restaurant2;
+    private Restaurant restaurant3;
     private String content;
     private int spicy;
     private String restaurantTags;
     private MultipartFile image;
+    private Review review1;
+    private Review review2;
+    private Review review3;
 
     @BeforeEach
     void setup() {
@@ -65,20 +76,20 @@ class ReviewServiceTest {
         userRepository.save(user1);
         userDetails1 = new UserDetailsImpl(user1);
 
-//        Location location2 = new Location("강북구", 34.31, 34.21);
-//        user1 = new User(
-//                "이한울",
-//                "a111",
-//                2L,
-//                "a@bs.com",
-//                UserRoleEnum.USER,
-//                2L,
-//                "default.png",
-//                location2,
-//                "별명이 뭔가요"
-//        );
-//        userRepository.save(user2);
-//        userDetails2 = new UserDetailsImpl(user2);
+        Location location2 = new Location("강북구", 34.31, 34.21);
+        user2 = new User(
+                "이한울",
+                "a111",
+                2L,
+                "a@bs.com",
+                UserRoleEnum.USER,
+                2L,
+                "default.png",
+                location2,
+                "별명이 뭔가요"
+        );
+        userRepository.save(user2);
+        userDetails2 = new UserDetailsImpl(user2);
 
         RestaurantSaveRequestDto restaurantSaveRequestDto1 = new RestaurantSaveRequestDto(
                 "엽떡",
@@ -104,7 +115,29 @@ class ReviewServiceTest {
                 "밀떡"
         );
         restaurant2 = new Restaurant(restaurantSaveRequestDto2, null, user1, location1);
+        restaurantRepository.save(restaurant2);
+        //리뷰저장
+        reviewRequestDto1 = new ReviewRequestDto(
+                "이집 매우매우 잘하네요.",
+                "1",
+                "2"
+        );
+        review1 = new Review(reviewRequestDto1, user1, restaurant1, null);
+        reviewRepository.save(review1);
 
+        reviewRequestDto2 = new ReviewRequestDto(
+                "이집 매우매우 별로네요.",
+                "5",
+                "3"
+        );
+        review2 = new Review(reviewRequestDto2, user1, restaurant1, "image1");
+        reviewRepository.save(review2);
+
+        reviewUpdateRequestDto1 = ReviewUpdateRequestDto.builder()
+                .content("매우매우맛이없어졌네요왜이래요")
+                .restaurantTags("1")
+                .spicy("5")
+                .build();
 
     }
 
@@ -116,17 +149,19 @@ class ReviewServiceTest {
         @DisplayName("리뷰 등록")
         void reviewCreate() {
             //given
-            reviewRequestDto = new ReviewRequestDto(
-                    "여기진짜진짜맛있어요!",
+            ReviewRequestDto reviewRequestDto = new ReviewRequestDto(
+                    "이집 매우매우 잘하네요.",
                     "1",
-                    "5"
+                    "2"
             );
+            review3 = new Review(reviewRequestDto, user1, restaurant2, null);
+            reviewRepository.save(review3);
             MockMultipartFile mockMultipartFile = new MockMultipartFile(
                     "image1", "image1", "application/doc", "image".getBytes()
             );
 
             //when
-            reviewService.createReview(restaurant1.getId(), reviewRequestDto, user1,mockMultipartFile);
+            reviewService.createReview(restaurant2.getId(), reviewRequestDto, user1, mockMultipartFile);
             //then
         }
 
@@ -134,20 +169,10 @@ class ReviewServiceTest {
         @DisplayName("리뷰 수정")
         void reviewUpdate() {
             //given
-            reviewRequestDto = new ReviewRequestDto(
-                    "여기완전완전맛있어요!",
-                    "5",
-                    "3"
+
+            Review review = reviewRepository.findById(review1.getId()).orElseThrow(
+                    ()-> new CustomException(ErrorCode.REVIEW_NOT_FOUND)
             );
-
-
-            Review review = Review.builder()
-                    .restaurant(restaurant1)
-                    .user(user1)
-                    .reviewRequestDto(reviewRequestDto)
-                    .imagePath("image1")
-                    .build();
-            reviewRepository.save(review);
 
             ReviewUpdateRequestDto reviewUpdateRequestDto = ReviewUpdateRequestDto.builder()
                     .content("매우매우맛이없어졌네요왜이래요")
@@ -155,12 +180,8 @@ class ReviewServiceTest {
                     .spicy("5")
                     .build();
 
-            MockMultipartFile mockMultipartFile = new MockMultipartFile(
-                    "image1", "image1", "application/doc", "image".getBytes()
-            );
             //when
-
-            review.updateReview(reviewUpdateRequestDto.getContent(),"image1",reviewUpdateRequestDto.getSpicy(),reviewUpdateRequestDto.getRestaurantTags());
+            review.updateReview(reviewUpdateRequestDto.getContent(), "image1", reviewUpdateRequestDto.getSpicy(), reviewUpdateRequestDto.getRestaurantTags());
             //then
             assertThat(review.getContent()).isEqualTo("매우매우맛이없어졌네요왜이래요");
             assertThat(review.getSpicy()).isEqualTo(5);
@@ -171,58 +192,40 @@ class ReviewServiceTest {
         @DisplayName("리뷰 삭제")
         void reviewDelete() {
             //given
-            reviewRequestDto = new ReviewRequestDto(
+            reviewRequestDto1 = new ReviewRequestDto(
                     "여기 되게되게 맛있어요!",
                     "1",
                     "2");
 
-            Review review = new Review(reviewRequestDto, user1, restaurant1,"image1");
+            Review review = new Review(reviewRequestDto1, user1, restaurant1,"image1");
             reviewRepository.save(review);
             //when
             reviewService.deleteReview(review.getId(), user1);
         }
 
+        //region 리뷰조회
+        @Test
+        @DisplayName("개인 리뷰 전체 조회")
+        void review() {
+            //given
+
+            List<Review> reviewList = reviewRepository.findAllByUserId(user1.getId());
+
+            assertThat(reviewList.size()).isEqualTo(2);
+
+        }
+        //endregion
+
 
         //region 리뷰전체조회
         @Test
-        @DisplayName("리뷰 전체 조회")
+        @DisplayName("식당 리뷰 전체 조회")
         void reviewList() {
             //given
-            List<ReviewAllResponseDto> reviewLists = new ArrayList<>();
-            reviewRequestDto = new ReviewRequestDto(
-                    "여기 되게되게 맛있어요!",
-                    "1",
-                    "2");
-            Review reviews = Review.builder()
-                    .restaurant(restaurant1)
-                    .user(user1)
-                    .reviewRequestDto(reviewRequestDto)
-                    .imagePath("image1")
-                    .build();
-            reviewRepository.save(reviews);
-            Review reviews2 = Review.builder()
-                    .restaurant(restaurant1)
-                    .user(user1)
-                    .reviewRequestDto(reviewRequestDto)
-                    .imagePath("image1")
-                    .build();
-            reviewRepository.save(reviews2);
-
 
             List<Review> reviewList = reviewRepository.findAllByRestaurantIdOrderByReviewLikeDesc(restaurant1.getId());
-            for(Review review : reviewList) {
-                ReviewAllResponseDto reviewAllResponseDto = ReviewAllResponseDto.builder()
-                        .reviewId(review.getId())
-                        .userId(review.getUser().getId())
-                        .restaurantId(review.getRestaurant().getId())
-                        .content(review.getContent())
-                        .reviewLikes(review.getReviewLike())
-                        .restaurantTags(review.getRestaurantTags())
-                        .image("image1")
-                        .build();
-                reviewLists.add(reviewAllResponseDto);
-            }
-            assertThat(reviewLists.size()).isEqualTo(2);
+
+            assertThat(reviewList.size()).isEqualTo(2);
 
         }
         //endregion
@@ -230,55 +233,46 @@ class ReviewServiceTest {
 
     //endregion
 
+    //region 실패케이스
+    @Nested
+    @DisplayName("실패케이스")
+    class fail {
+        @Test
+        @DisplayName("리뷰 등록 - 식당이 없을 때")
+        void 식당없음() {
+            //given
 
-//region 성공케이스
-//    @Nested
-//    @DisplayName("실패케이스")
-//    class fail {
-//        @Test
-//        @DisplayName("음식점 null")
-//        void 음식점없음() {
-//            //given
-//
-//            reviewRequestDto = new ReviewRequestDto(
-//                    "여기맛있어요!",
-//                    1,
-//                    "진짜 맛있네요!",
-//                    null);
-//
-//            //when
-//            reviewService.createReview(restaurant2.getId(), reviewRequestDto, user1);
-//
-//            //then
-//            assertThat(restaurant2.getId()).isEqualTo("해당 음식점이 존재하지 않습니다.");
-//        }
-//
-//        @Test
-//        @DisplayName("리뷰 수정")
-//        void 리뷰수정() {
-//            //given
-//            reviewRequestDto = new ReviewRequestDto(
-//                    "여기맛있어요!",
-//                    1,
-//                    "진짜 맛있네요!",
-//                    null
-//            );
-//
-//            Review review = new Review(reviewRequestDto, user1, restaurant1);
-//            reviewRepository.save(review);
-//
-//            ReviewUpdateRequestDto reviewUpdateRequestDto = new ReviewUpdateRequestDto(
-//                    "별로네요",
-//                    100,
-//                    "흠..",
-//                    null
-//            );
-//            reviewService.updateReview(review.getId(), reviewUpdateRequestDto, user1);
-//
-//            //then
-//            assertThat(review.getContent()).isEqualTo("별로네요");
-//            assertThat(review.getSpicy()).isEqualTo(100);
-//            assertThat(review.getRestaurantTags()).isEqualTo("흠..");
-//        }
-//    }
+            //when
+            CustomException exception = assertThrows(CustomException.class,
+                    ()->reviewService.createReview(0L,reviewRequestDto1,user1,null));
+
+            assertEquals(exception.getErrorCode().getDetail(),"해당 음식점이 존재하지 않습니다.");
+        }
+        @Test
+        @DisplayName("리뷰 수정 - 리뷰가 없을 때")
+        void 리뷰없음() {
+            //given
+
+            //when
+            CustomException exception = assertThrows(CustomException.class,
+                    ()->reviewService.updateReview(restaurant1.getId(),reviewUpdateRequestDto1,user1,null));
+
+            assertThat(exception.getErrorCode().getDetail()).isEqualTo("해당 리뷰가 존재하지 않습니다.");
+        }
+
+        @Test
+        @DisplayName("리뷰 삭제 - 사용자가 다를 때")
+        void 삭제사용자다름() {
+            //given
+
+            //when
+            Exception exception = assertThrows(IllegalArgumentException.class,
+                    ()->reviewService.deleteReview(review1.getId(),user2));
+
+            assertThat(exception.getMessage()).isEqualTo("댓글의 작성자만 삭제가 가능합니다.");
+        }
+    }
+    //endregion
+
+
 }
