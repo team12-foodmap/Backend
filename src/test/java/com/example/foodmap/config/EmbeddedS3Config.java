86309c27f519e93588ad915d3d74773226ad2b94
joorani <80900915+joorani@users.noreply.github.com>
2022-01-12 18:52:50 +0900

    test code 추가 및 필요없는 파일 삭제  (#9)
    * feat: 식당 유효성 검사 및 test code 추가
    
    * feat: 식당, 이미지 저장 test code 추가
    
    * fix: 필요없는 파일 제거

--- src/test/java/com/example/foodmap/config/EmbeddedS3Config.java
+++ src/test/java/com/example/foodmap/config/EmbeddedS3Config.java
@@ -1,67 +1,55 @@
-package com.example.foodmap.config;
-
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.AnonymousAWSCredentials;
-import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import io.findify.s3mock.S3Mock;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-import java.io.IOException;
-
-@Slf4j
-@Profile({"local"})
-@Configuration
-public class EmbeddedS3Config {
-    @Value("${cloud.aws.region.static}")
-    private String region;
-    @Value("${cloud.aws.s3.bucket}")
-    private String bucket;
-    @Value("${cloud.aws.s3.mock.port}")
-    private int port;
-    @Bean
-    public S3Mock s3Mock() {
-        return new S3Mock.Builder()
-                .withPort(port) // 해당 포트에 프로세스가 생성된다.
-                .withInMemoryBackend() // 인메모리에서 활성화.
-                .build(); }
-    @PostConstruct // 의존성 주입이 이루어진 후 임베디드 S3 서버를 가동시킨다.
-    public void startS3Mock() throws IOException {
-        port = ProcessUtils.isRunningPort(port) ? ProcessUtils.findAvailableRandomPort() : port;
-        this.s3Mock().start();
-        log.info("인메모리 S3 Mock 서버가 시작됩니다. port: {}", port); }
-
-    @PreDestroy
-    public void destroyS3Mock() {
-        this.s3Mock().shutdown();
-        log.info("인메모리 S3 Mock 서버가 종료됩니다. port: {}", port); }
-    @Bean
-    @Primary
-    public AmazonS3 amazonS3Client() {
-        AwsClientBuilder.EndpointConfiguration endpoint = new AwsClientBuilder.EndpointConfiguration(getUri(), region);
-        AmazonS3 client = AmazonS3ClientBuilder
-                .standard()
-                .withPathStyleAccessEnabled(true)
-                .withEndpointConfiguration(endpoint)
-                .withCredentials(new AWSStaticCredentialsProvider(new AnonymousAWSCredentials()))
-                .build();
-        client.createBucket(bucket);
-        return client; }
-
-    private String getUri() {
-        return UriComponentsBuilder.newInstance()
-                .scheme("http")
-                .host("localhost")
-                .port(port)
-                .build()
-                .toUriString(); }
-}
+//package com.example.foodmap.config;
+//
+//import com.amazonaws.auth.AWSStaticCredentialsProvider;
+//import com.amazonaws.auth.AnonymousAWSCredentials;
+//import com.amazonaws.client.builder.AwsClientBuilder;
+//import com.amazonaws.services.s3.AmazonS3;
+//import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+//import io.findify.s3mock.S3Mock;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.test.context.TestConfiguration;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Primary;
+//
+//@Slf4j
+//@TestConfiguration
+//public class EmbeddedS3Config {
+//
+//    //@Value는 Properties에서 값을 가져온다.
+//    @Value("${cloud.aws.region.static}")
+//    String region;
+//
+//    @Value("${cloud.aws.s3.bucket}")
+//    String bucket;
+//
+//    int port = 8001;
+//
+//    //S3Mock을 빌드할때 포트나 메모리에 저장할 지 실제로 저장할 지 같은 것 등등을 설정 가능하다.
+//    @Bean
+//    public S3Mock s3Mock() {
+//        return new S3Mock.Builder()
+//                .withPort(port)
+//                .withInMemoryBackend()
+//                .build();
+//    }
+//
+//    //위에서 작성한 S3Mock을 주입받는 Bean을 작성하였다.
+//    //실제 테스트가 아닌 환경을 위해 작성된 Config환경과 같이 켜질 경우를 대비하여 @Primary를 넣어주었다.
+//    //s3Mock.start를 이용하여 Mock S3 서버를 로컬에서 시작한다.
+//    @Bean
+//    @Primary
+//    public AmazonS3 amazonS3(S3Mock s3Mock){
+//        s3Mock.start();
+//        AwsClientBuilder.EndpointConfiguration endpoint = new AwsClientBuilder.EndpointConfiguration("http://localhost:8001", region);
+//        AmazonS3 client = AmazonS3ClientBuilder
+//                .standard()
+//                .withPathStyleAccessEnabled(true)
+//                .withEndpointConfiguration(endpoint)
+//                .withCredentials(new AWSStaticCredentialsProvider(new AnonymousAWSCredentials()))
+//                .build();
+//        client.createBucket(bucket);
+//
+//        return client;
+//    }
+//}
