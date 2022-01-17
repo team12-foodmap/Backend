bcbc7692c8d31c357bd75b0efb42b89893a24965
goodn911 <90505402+goodn911@users.noreply.github.com>
2022-01-17 22:53:38 +0900

    fix : logback 주석

--- src/main/java/com/example/foodmap/config/WebConfig.java
+++ src/main/java/com/example/foodmap/config/WebConfig.java
@@ -1,25 +1,25 @@
-
-package com.example.foodmap.config;
-
-
-import com.example.foodmap.exception.LogInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-    private final LogInterceptor logInterceptor;
-
-    public WebConfig(LogInterceptor logInterceptor) {
-        this.logInterceptor = logInterceptor;
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(logInterceptor).addPathPatterns("/**")
-                .excludePathPatterns("/css/**")
-                .excludePathPatterns("/js/**")
-                .excludePathPatterns("/index.html");
-    }
-}
+//
+//package com.example.foodmap.config;
+//
+//
+//import com.example.foodmap.exception.LogInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//    private final LogInterceptor logInterceptor;
+//
+//    public WebConfig(LogInterceptor logInterceptor) {
+//        this.logInterceptor = logInterceptor;
+//    }
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(logInterceptor).addPathPatterns("/**")
+//                .excludePathPatterns("/css/**")
+//                .excludePathPatterns("/js/**")
+//                .excludePathPatterns("/index.html");
+//    }
+//}
