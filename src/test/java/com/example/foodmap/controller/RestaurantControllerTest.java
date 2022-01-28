3a1da390decb25068871b990726e4589b661e81c
joorani <shinerani16@gmail.com>
2022-01-28 11:36:40 +0900

    test: test 추가

--- src/test/java/com/example/foodmap/controller/RestaurantControllerTest.java
+++ src/test/java/com/example/foodmap/controller/RestaurantControllerTest.java
@@ -9,43 +9,31 @@ import com.example.foodmap.security.UserDetailsImpl;
 import com.example.foodmap.security.WebSecurityConfig;
 import com.example.foodmap.service.RedisService;
 import com.example.foodmap.service.RestaurantService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.awt.*;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
 import java.security.Principal;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(
--- src/test/java/com/example/foodmap/service/StorageServiceTest.java
+++ src/test/java/com/example/foodmap/service/StorageServiceTest.java
@@ -1,48 +1,43 @@
-//package com.example.foodmap.service;
-//
-//import com.example.foodmap.config.EmbeddedS3Config;
-//import io.findify.s3mock.S3Mock;
-//import org.aspectj.lang.annotation.After;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.TestInstance;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.context.annotation.Import;
-//import org.springframework.mock.web.MockMultipartFile;
-//import org.springframework.test.annotation.Rollback;
-//import org.springframework.test.context.ActiveProfiles;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import java.io.IOException;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//
-//
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//@ActiveProfiles("test")
-//@Import(EmbeddedS3Config.class)
-//class StorageServiceTest {
-//    @Autowired
-//    S3Mock s3Mock;
-//
-//    @Autowired
-//    StorageService storageService;
-//
-//    @Test
-//    public void test() throws IOException {
-//        String expected = "mock1.png";
-//        MockMultipartFile mockMultipartFile = new MockMultipartFile("file", expected,
-//                "image/png", "test data".getBytes());
-//        String imagePath = storageService.uploadFile(mockMultipartFile, "restaurant");
-//
-//        assertThat(mockMultipartFile.getOriginalFilename()).isEqualTo(expected);
-//    }
-//
-//    @After("")
-//    public void shutdownMockS3(){
-//        s3Mock.stop();
-//    }
-//}
+package com.example.foodmap.service;
+
+import com.example.foodmap.config.EmbeddedS3Config;
+import io.findify.s3mock.S3Mock;
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@Import(EmbeddedS3Config.class)
+class StorageServiceTest {
+    @Autowired
+    S3Mock s3Mock;
+
+    @Autowired
+    StorageService storageService;
+
+    @Test
+    public void test() throws IOException {
+        String expected = "mock1.png";
+        MockMultipartFile mockMultipartFile = new MockMultipartFile("file", expected,
+                "image/png", "test data".getBytes());
+        String imagePath = storageService.uploadFile(mockMultipartFile, "restaurant");
+
+        assertThat(mockMultipartFile.getOriginalFilename()).isEqualTo(expected);
+    }
+
+    @After("")
+    public void shutdownMockS3(){
+        s3Mock.stop();
+    }
+}
